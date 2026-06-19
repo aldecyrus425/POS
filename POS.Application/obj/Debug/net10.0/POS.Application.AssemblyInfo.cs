f3b8cf41623da5a973e6b7528a64742b27b50b65
Cyrus <Aldecyrus425@gmail.com>
2026-06-19 13:44:34 +0800

    Local Commit

--- POS.Application/obj/Debug/net10.0/POS.Application.AssemblyInfo.cs
+++ POS.Application/obj/Debug/net10.0/POS.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40331cd3dbb53c0ae7b2d038f97286ca4d9fa540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f067b933c5fe3a94be9383e4ab8a52e72cc2446d")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
